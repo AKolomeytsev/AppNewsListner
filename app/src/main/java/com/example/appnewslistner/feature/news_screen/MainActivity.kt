5203191c283698ce57ed6ffd8d8012f2a5b8313f
Artem <a.kolomeytsev@list.ru>
2023-03-14 15:26:12 +0300

    --Создан фрагмент, все запустилось

--- app/src/main/java/com/example/appnewslistner/feature/news_screen/MainActivity.kt
+++ app/src/main/java/com/example/appnewslistner/feature/news_screen/MainActivity.kt
@@ -1,11 +1,16 @@
-package com.example.appnewslistner
+package com.example.appnewslistner.feature.news_screen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.appnewslistner.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.screensContener, MainScreenFragment())
+            .commit()
     }
 }
---
+++ app/src/main/java/com/example/appnewslistner/feature/news_screen/MainScreenFragment.kt
@@ -0,0 +1,9 @@
+package com.example.appnewslistner.feature.news_screen
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.appnewslistner.R
+
+class MainScreenFragment:Fragment(R.layout.fragment_main_screen) {
+
+}

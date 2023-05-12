d540c968d5e14f56749a7a93a8b77556475e7ce1
Artem <a.kolomeytsev@list.ru>
2023-05-12 15:58:55 +0300

    --Визуализация списков новостей

--- app/src/main/java/com/example/appnewslistner/feature/news_screen/MainScreenFragment.kt
+++ app/src/main/java/com/example/appnewslistner/feature/news_screen/MainScreenFragment.kt
@@ -2,19 +2,24 @@ package com.example.appnewslistner.feature.news_screen
 
 import android.os.Bundle
 import android.view.View
-import android.widget.TextView
+
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import com.example.appnewslistner.R
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainScreenFragment:Fragment(R.layout.fragment_main_screen) {
     private val viewModel:MainScreenViewModel by viewModel()
-    private val text: TextView by lazy { requireActivity().findViewById(R.id.text1_fragment_main_screen) }
+    private val recyclerView:RecyclerView by lazy { requireActivity().findViewById(R.id.listView) }
+    private val adapter:ArticlesAdapter by lazy { ArticlesAdapter() }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel.viewState.observe(viewLifecycleOwner,::render)
+        recyclerView.adapter = adapter
     }
     private fun render(viewState:ViewState){
-        text.text = viewState.articles.toString();
+        adapter.setDate(viewState.articles)
+
     }
 }

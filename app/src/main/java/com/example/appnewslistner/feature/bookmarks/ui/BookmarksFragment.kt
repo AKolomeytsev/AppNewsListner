0dfafed25d1d20d021302d9ded5ceb78e60d7006
Artem <a.kolomeytsev@list.ru>
2023-05-15 12:30:43 +0300

    --Создание второго экрана

---
+++ app/src/main/java/com/example/appnewslistner/feature/bookmarks/ui/BookmarksFragment.kt
@@ -0,0 +1,7 @@
+package com.example.appnewslistner.feature.bookmarks.ui
+
+import androidx.fragment.app.Fragment
+import com.example.appnewslistner.R
+
+class BookmarksFragment:Fragment(R.layout.fragment_bookmarks) {
+}
